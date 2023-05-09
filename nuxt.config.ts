1a5945f7b45af75db6f3007b8a6161936e2f5f42
Hoshiko <evo960225@gmail.com>
2023-05-09 22:21:54 +0800

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/i18n',
+  ],
+  runtimeConfig: {
+    public: {
+    }
+  },
+  css: ['@/assets/styles/main.scss'],
+})
